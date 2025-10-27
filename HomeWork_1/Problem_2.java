0d148cff3dbbd8c29f060571d78bf769f06adc9d
Tofayel Ahmmed <tofayel.ahmmed084@gmail.com>
2025-10-27 22:50:05 +0600

    Updated From txt to java

--- HomeWork_1/Problem_2.java
+++ HomeWork_1/Problem_2.java
@@ -1,15 +1,15 @@
-package practice_application;
-   import java.util.*;
 
-public class Practice_Application {
+import java.util.*;
+
+public class Problem_2 {
 
     public static void main(String[] args) {
-   
+
         Scanner input = new Scanner(System.in);
-      
+
         System.out.print("Enter your name: ");
         String name = input.nextLine();
         System.out.println("Hello," + name + "!");
-        
+
     }
 }
